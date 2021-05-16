65361438d28ee7a2957da18b6359222132b10423
giovani-almeida <giovanialmedini@hotmail.com>
2021-05-15 23:24:14 -0300

    Criação da listar de usuários e da web página.

--- src/main/java/controller/usuariocontroller.java
+++ src/main/java/controller/usuariocontroller.java
@@ -1,4 +1,14 @@
-package controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class usuariocontroller {
+
+    @RequestMapping("/")
+    public String listarUsuarios() {
+
+        System.out.println("USUARIOS");
+
+        return "usuarios";
+    }
 }
