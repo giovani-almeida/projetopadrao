4840eeb0088461e770d24df735ad4c4275506a38
giovani-almeida <giovanialmedini@hotmail.com>
2021-05-16 18:55:53 -0300

    Criação do controlador Model.

---
+++ src/main/java/com/senac/projetopadrao/model/Usuario.java
@@ -0,0 +1,17 @@
+package com.senac.projetopadrao.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    }
