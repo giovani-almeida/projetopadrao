068b94e379191cfc617b9697fb4acb0ab5d3a701
giovani-almeida <giovanialmedini@hotmail.com>
2021-05-19 16:41:52 -0300

    Ajustes Usuario.Java ControllerRest.java e html

--- src/main/java/com/senac/projetopadrao/controller/controllersRest/UsuarioControllerRest.java
+++ src/main/java/com/senac/projetopadrao/controller/controllersRest/UsuarioControllerRest.java
@@ -1,63 +1,36 @@
-package com.senac.projetopadrao.controller.controllersRest;
-
-import com.senac.projetopadrao.model.Usuario;
+package com.senac.projetopadrao.controllersRest;
+import com.senac.projetopadrao.models.Usuario;
 import com.senac.projetopadrao.repositorys.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-
-@SuppressWarnings("ALL")
+import java.util.List;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioControllerRest {
-
     @Autowired
     UsuarioRepository usuarioRepository;
+    @GetMapping("/listar")
+    public ArrayList<Usuario> listar(){
+        ArrayList<Usuario> usuarios = new ArrayList<>();
+        usuarios = (ArrayList<Usuario>) usuarioRepository.findAll();
+        return usuarios;
+    }
 
     @PostMapping("/add")
+    @PostMapping("/addRest")
     public String addUsuario(){
 
         Usuario usuario = new Usuario();
-        usuario.setNome("Giovani");
+        usuario.setNome("pedro");
+        usuario.setDataDeNascimento("07/08/1987");
+        usuario.setEstadoCivil(true);
         usuarioRepository.save(usuario);
-
         return "usuario adicionado";
     }
-
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.web.bind.annotation;
-    import java.lang.reflect.Array;
-    import java.util.ArrayList;
-    import java.util.List;
-
-    @RestController
-    @RequestMapping("/usuarios")
-    public class UsuarioControllerRest {
-
+    @PutMapping("/editar")
+    public String editarUsuario(){
+        return "usuario editado";
     }
-        @Autowired
-        UsuarioRepository usuarioRepository;
-
-        @GetMapping("/listar")
-        public ArrayList<Usuario> listar(){
-            ArrayList<Usuario> usuarios = new ArrayList<>();
-            usuarios = (ArrayList<Usuario>) usuarioRepository.findAll();
-            return usuarios;
-        }
-
-        @PostMapping({"/add", "/addRest"})
-        public String addUsuario(){
-
-            Usuario usuario = new Usuario();
-            usuario.setNome("john");
-            usuario.setDataDeNascimento("08/10/2001");
-            usuario.setEstadoCivil(true);
-            usuarioRepository.save(usuario);
-            return "usuario adicionado";
-        }
-        @PutMapping("/editar")
-        public String editarUsuario(){
-            return "usuario editado";
-        }
-    }
+}
